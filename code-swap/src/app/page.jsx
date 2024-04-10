f173c973f48a66fd254661222d42c11b33456872
Shepardy22 <inv72kmaster@gmail.com>
2024-04-10 17:21:52 -0300

    Reset

--- code-swap/src/app/page.jsx
+++ code-swap/src/app/page.jsx
@@ -1,18 +1,4 @@
-import Carreiras from "./components/HomePage/Carreiras";
-import Home from "./components/HomePage/Home";
 
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
-import Metodologia from "./components/HomePage/Metodologia";
-import Comunidade from "./components/HomePage/Comunidade";
-import SaibaMais from "./components/HomePage/SaibaMais";
-import Login from "./components/HomePage/Login";
-import Cadastro from "./components/HomePage/Cadastro";
-
-import Dashboard from "./components/Private/Dashboard";
-import { AuthProvider } from "./contexts/authVerify";
-import { PrivateRoutes } from "./components/PrivateRoutes";
-import { ContextStore } from "./contexts/contextProject";
-import AdmUser from "./components/Private/AdmUser";
 
 
 export default function Home() {
