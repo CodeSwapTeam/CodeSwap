f39d6b6e6a8a51f2039b4755246e8d9f0b518094
Shepardy22 <inv72kmaster@gmail.com>
2024-04-10 16:40:51 -0300

    LogsRegister

--- code-swap/src/app/layout.js
+++ code-swap/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { InteractionProvider } from "./contexts/InteractionContext";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="Pt-br">
-      <body className={inter.className}>{children}</body>
+      <InteractionProvider>
+        <body className={inter.className}>{children}</body>
+      </InteractionProvider>
+      
     </html>
   );
 }
