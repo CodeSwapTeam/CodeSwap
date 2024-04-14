e3a14b8f08aced9702d241f86b2fac1fe4b755d7
shepardy22 <inv72kmaster@gmail.com>
2024-04-14 09:54:14 -0300

    Autenticação e rota protegida

--- code-swap/src/app/Dashboard/page.jsx
+++ code-swap/src/app/Dashboard/page.jsx
@@ -28,7 +28,7 @@ export default function Dashboard(){
     }
 
     useEffect(()=>{
-       // console.log('daboard', currentUser);
+       
     },[])
 
     return(
--- code-swap/src/app/login/page.jsx
+++ code-swap/src/app/login/page.jsx
@@ -3,91 +3,83 @@ import { useEffect, useState } from "react";
 import { useAuthContext } from "../contexts/Auth";
 import { useRouter } from "next/navigation";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth , } from "../../../database/firebase";
-import {setCookies} from "../services/cookies";
+import { auth, } from "../../../database/firebase";
+import { setCookies } from "../services/cookies";
 
 
 
-export default function Login(){
+export default function Login() {
 
-    const {currentUser, setCurrentUser} = useAuthContext();
+    const { currentUser, setCurrentUser } = useAuthContext();
     const r = useRouter();
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
-  useEffect(()=> {
-    //console.log('login', currentUser);
-    if(currentUser ){
-        r.push('/Dashboard');
-    }
-   
-    
-  },[currentUser])
+    useEffect(() => {
+        //console.log('login', currentUser);
+        if (currentUser) {
+            r.push('/Dashboard');
+        }
 
-    
-   
 
-  
+    }, [currentUser])
+
+
     const handleSubmit = async (e) => {
-      
-      e.preventDefault();
 
-      
-      
-      
-      try {
-        const userCredential = await signInWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
-        
-        //localStorage.setItem('userId', user.uid);
-        setCookies(user.accessToken);
-        localStorage.setItem('user',user.accessToken);
-        // Limpar os campos de e-mail e senha após a autenticação do usuário
-        setEmail('');
-        setPassword('');
-        setError('');
-        //console.log(user);
-        setCurrentUser(user);
-        
-       // router.push('/Dashboard');
-        
-      } catch (error) {
-        setError(error.message);
-      }
+        e.preventDefault();
+
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+
+            //localStorage.setItem('userId', user.uid);
+            setCookies(user.accessToken);
+            localStorage.setItem('user', user.accessToken);
+
+            setEmail('');
+            setPassword('');
+            setError('');
+            //console.log(user);
+            setCurrentUser(user);;
+
+        } catch (error) {
+            setError(error.message);
+        }
     };
-    
-      return (
+
+    return (
         <div style={{ maxWidth: '400px', margin: '0 auto', padding: '20px', border: '1px solid #ccc', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
-           <h2 style={{ textAlign: 'center' }}>Authentication with Email and Password</h2>
-           <form onSubmit={handleSubmit}>
-             <div style={{ marginBottom: '20px' }}>
-               <label htmlFor="email" style={{ display: 'block', marginBottom: '5px' }}>Email:</label>
-               <input 
-                 type="email" 
-                 id="email" 
-                 value={email} 
-                 onChange={(e) => setEmail(e.target.value)} 
-                 required 
-                 style={{ width: '100%', padding: '8px', border: '1px solid #ccc', borderRadius: '4px' }} 
-               />
-             </div>
-             <div style={{ marginBottom: '20px' }}>
-               <label htmlFor="password" style={{ display: 'block', marginBottom: '5px' }}>Password:</label>
-               <input 
-                 type="password" 
-                 id="password" 
-                 value={password} 
-                 onChange={(e) => setPassword(e.target.value)} 
-                 required 
-                 style={{ width: '100%', padding: '8px', border: '1px solid #ccc', borderRadius: '4px' }} 
-               />
-             </div>
-             <button type="submit" style={{ width: '100%', padding: '10px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>Authenticate</button>
-           </form>
-           {error && <p style={{ color: 'red', marginTop: '10px' }}>Error: {error}</p>}
-         </div>
-     )
+            <h2 style={{ textAlign: 'center' }}>Authentication with Email and Password</h2>
+            <form onSubmit={handleSubmit}>
+                <div style={{ marginBottom: '20px' }}>
+                    <label htmlFor="email" style={{ display: 'block', marginBottom: '5px' }}>Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        style={{ width: '100%', padding: '8px', border: '1px solid #ccc', borderRadius: '4px' }}
+                    />
+                </div>
+                <div style={{ marginBottom: '20px' }}>
+                    <label htmlFor="password" style={{ display: 'block', marginBottom: '5px' }}>Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        style={{ width: '100%', padding: '8px', border: '1px solid #ccc', borderRadius: '4px' }}
+                    />
+                </div>
+                <button type="submit" style={{ width: '100%', padding: '10px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>Authenticate</button>
+            </form>
+            {error && <p style={{ color: 'red', marginTop: '10px' }}>Error: {error}</p>}
+        </div>
+    )
 
 }
